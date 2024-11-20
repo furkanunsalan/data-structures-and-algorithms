8cf89392997cafb234ce7ab94e7a3195ab89f912
Furkan Ünsalan <me@furkanunsalan.dev>
2024-11-20 23:43:29 +0300

    feat(algo): changed mergo sort to geeks for geeks

--- merge-sort/ms.py
+++ merge-sort/ms.py
@@ -1,35 +1,66 @@
-def merge_sort(arr):
-    if len(arr) > 1:
-        mid = len(arr) // 2
-        left_half = arr[:mid]
-        right_half = arr[mid:]
+def merge(arr, left, mid, right):
+    n1 = mid - left + 1
+    n2 = right - mid
 
-        merge_sort(left_half)
-        merge_sort(right_half)
+    # Create temp arrays
+    L = [0] * n1
+    R = [0] * n2
 
-        i = j = k = 0
+    # Copy data to temp arrays L[] and R[]
+    for i in range(n1):
+        L[i] = arr[left + i]
+    for j in range(n2):
+        R[j] = arr[mid + 1 + j]
 
-        while i < len(left_half) and j < len(right_half):
-            if left_half[i] < right_half[j]:
-                arr[k] = left_half[i]
-                i += 1
-            else:
-                arr[k] = right_half[j]
-                j += 1
-            k += 1
+    i = 0  # Initial index of first subarray
+    j = 0  # Initial index of second subarray
+    k = left  # Initial index of merged subarray
 
-        while i < len(left_half):
-            arr[k] = left_half[i]
+    # Merge the temp arrays back
+    # into arr[left..right]
+    while i < n1 and j < n2:
+        if L[i] <= R[j]:
+            arr[k] = L[i]
             i += 1
-            k += 1
-
-        while j < len(right_half):
-            arr[k] = right_half[j]
+        else:
+            arr[k] = R[j]
             j += 1
-            k += 1
+        k += 1
 
-# Example usage
-arr = [38, 27, 43, 3, 9, 82, 10]
-merge_sort(arr)
-print("Sorted array:", arr)
+    # Copy the remaining elements of L[],
+    # if there are any
+    while i < n1:
+        arr[k] = L[i]
+        i += 1
+        k += 1
 
+    # Copy the remaining elements of R[], 
+    # if there are any
+    while j < n2:
+        arr[k] = R[j]
+        j += 1
+        k += 1
+
+def merge_sort(arr, left, right):
+    if left < right:
+        mid = (left + right) // 2
+
+        merge_sort(arr, left, mid)
+        merge_sort(arr, mid + 1, right)
+        merge(arr, left, mid, right)
+
+def print_list(arr):
+    for i in arr:
+        print(i, end=" ")
+    print()
+
+# Driver code
+if __name__ == "__main__":
+    arr = [12, 11, 13, 5, 6, 7]
+    print("Given array is")
+    print_list(arr)
+
+    merge_sort(arr, 0, len(arr) - 1)
+
+    print("\nSorted array is")
+    print_list(arr)
